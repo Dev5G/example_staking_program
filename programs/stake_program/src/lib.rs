f7e9747b4028e54966fe46a0ebdd2ee1e29f91a9
Shahid H. <shahid.sgt@outlook.com>
2025-09-01 10:40:54 +0500

    Added the unstake function call

--- programs/stake_program/src/lib.rs
+++ programs/stake_program/src/lib.rs
@@ -11,6 +11,26 @@ use instructions::*;
 
 declare_id!("79Wno3MHuSPo9dWQSDdXK1woSPx8ndHhqRmbpfCvhvdp");
 
+#[cfg(not(feature = "no-entrypoint"))]
+use {solana_security_txt::security_txt};
+
+#[cfg(not(feature = "no-entrypoint"))]
+security_txt! {
+    name: "Bawls Stake Program",
+    project_url: "https://bawlsonu.life",
+    contacts: "email:security@myproject.com,discord:security#1234",
+    policy: "https://myproject.com/security-policy",
+
+    // Optional Fields
+    preferred_languages: "en,de",
+    source_code: "https://github.com/solana-developers/solana-game-preset",
+    source_revision: "5vJwnLeyjV8uNJSp1zn7VLW8GwiQbcsQbGaVSwRmkE4r",
+    source_release: "",
+    encryption: "",
+    auditors: "Verifier pubkey: 5vJwnLeyjV8uNJSp1zn7VLW8GwiQbcsQbGaVSwRmkE4r",
+    acknowledgements: "Thank you to our bug bounty hunters!"
+}
+
 #[program]
 pub mod stake_program {
     use super::*;
@@ -27,9 +47,9 @@ pub mod stake_program {
         instructions::stake::stake_handler(ctx, amount)
     }
 
-    // pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
-    //     instructions::unstake::handler(ctx)
-    // }
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
+        instructions::unstake::unstake_handler(ctx)
+    }
 
     // pub fn claim_rewards(ctx: Context<ClaimRewards>) -> Result<()> {
     //     instructions::claim_rewards::handler(ctx)
